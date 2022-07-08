11f04199a4d575c6e95c35f420aa81125e7189a7
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-08 17:57:22 +0200

    start: started creating img  div with item div from js, just starting for

---
+++ index.js
@@ -0,0 +1,12 @@
+const imgArray = [
+    "img/01.jpg", 
+    "img/02.jpg", 
+    "img/03.jpg", 
+    "img/04.jpg"
+]
+
+const itemsDom = document.querySelector ('items')
+
+for [ let i = 0; i < imgArray.length; i++] {
+    
+} 
