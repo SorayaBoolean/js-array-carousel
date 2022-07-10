6f7b7d2a2a0c76fba50ab679bd284e07ece1684f
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-10 17:33:29 +0200

    js add: added function for next from quert selector next and query selector previous and then removed and added classlist show by increasing or decreasing slides

--- index.js
+++ index.js
@@ -5,13 +5,44 @@ const imgArray = [
     "img/04.jpg"
 ]
 
-const itemsDom = document.querySelector ('items')
+let slideAttiva = 0;
+//riproduco l' array di immagini attraverso javascript //
 
-for [ let i = 0; i < imgArray.length; i++] {
-    itemsDom.innerHTML += `<div class="item"> 
+const itemsDom = document.querySelector ('.items') ;
+
+for ( let i = 0; i < imgArray.length; i++) {
+    itemsDom.innerHTML +=
+    ` <div class="item"> 
     <img class="img-carousel" src="${imgArray [i]}/>"
-    </div>` ;
+    </div>`;
 } 
 
-const ItemList = document.getElementsByClassName ('item');
-                
+//partendo dall elemento 0 dell array di img gli attribuisco show//
+const itemList = document.getElementsByClassName ('item');
+console.log(itemList)
+ 
+itemList [slideAttiva].classList.add('show');
+
+//aggiungo event listner per div next
+
+const next = document.querySelector ('.next')
+
+next.addEventListener ('click',
+    function () {
+        itemList [slideAttiva].classList.remove ('show');
+        slideAttiva++ ;
+        itemList [slideAttiva].classList.add('show');
+
+    }
+)
+
+const previous = document.querySelector ('.previous')
+
+previous.addEventListener ('click',
+    function () {
+        itemList [slideAttiva].classList.remove ('show');
+        slideAttiva-- ;
+        itemList [slideAttiva].classList.add('show');
+
+    }
+)
